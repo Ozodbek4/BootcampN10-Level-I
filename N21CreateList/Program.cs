f5b5ca08468d4490e54ede447428c1107a8adda1
Ozodbek4 <anvarjonovozodbek416@gmail.com>
2023-08-10 17:04:54 +0500

    Home

---
+++ N21CreateList/Program.cs
@@ -0,0 +1,11 @@
+﻿List<int> builtList = new List<int>();
+CustomList<int> customList = new CustomList<int>();
+
+public class CustomList<IList>
+{
+
+    public IList Add(IList itemA, IList itemB)
+    {
+        return (IList)itemA + (IList)itemB;
+    }
+}
