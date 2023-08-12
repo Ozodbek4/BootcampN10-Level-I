ed9883333b530fe0522a9369d0cd0bccc389cf48
Ozodbek4 <anvarjonovozodbek416@gmail.com>
2023-08-12 16:22:09 +0500

    HomeTask23 full

---
+++ N23Home2/Program.cs
@@ -0,0 +1,30 @@
+﻿using N23Home2;
+
+var users = new List<User>
+{
+    new User("John", "Doe", "johndoe@example.com"),
+    new User("Jane", "Doe", "janedoe@example.com"),
+    new User("Bob", "Smith", "bobsmith@example.com"),
+    new User("Alice", "Johnson", "alicejohnson@example.com"),
+    new User("Mike", "Brown", "mikebrown@example.com"),
+    new User("Emily", "Davis", "emilydavis@example.com"),
+    new User("David", "Wilson", "davidwilson@example.com"),
+    new User("Sarah", "Taylor", "sarahtaylor@example.com"),
+    new User("Tom", "Anderson", "tomanderson@example.com"),
+    new User("Lisa", "Thomas", "lisathomas@example.com")
+};
+
+
+Console.WriteLine("Kerakli user uchun kalit so'zni kiriting: ");
+var needed = Console.ReadLine();
+
+//var re = users.Select(user =>
+//{
+//    if (user.FistName.Contains(needed, StringComparison.OrdinalIgnoreCase) || user.LastName.Contains(needed, StringComparison.OrdinalIgnoreCase) || user.EmailAddress.Contains(needed, StringComparison.OrdinalIgnoreCase){
+//        return user;
+//    }
+//});
+
+var result = users.Where(user => user.FistName.Contains(needed, StringComparison.OrdinalIgnoreCase) || user.LastName.Contains(needed, StringComparison.OrdinalIgnoreCase) || user.EmailAddress.Contains(needed, StringComparison.OrdinalIgnoreCase));
+
+result.ToList().ForEach(user => Console.WriteLine($"{user.FistName.PadRight(15)}{user.LastName.PadRight(15)}{user.EmailAddress}"));
---
+++ N23Home2/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace N23Home2
+{
+    internal class User
+    {
+        public string FistName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+
+        public User(string fistName, string lastName, string emailAddress)
+        {
+            FistName = fistName;
+            LastName = lastName;
+            EmailAddress = emailAddress;
+        }
+    }
+}
