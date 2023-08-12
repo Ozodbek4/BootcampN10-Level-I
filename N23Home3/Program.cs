4984d1cb59251bb7027c134962a285afa80b7159
Ozodbek4 <anvarjonovozodbek416@gmail.com>
2023-08-12 17:18:42 +0500

    Tasks

---
+++ N23Home3/Program.cs
@@ -0,0 +1,13 @@
+﻿List<int> numbers = new List<int>()
+{
+    322,
+    -1,
+    11,
+    3,
+    6,
+    10
+};
+
+Console.WriteLine($"Hammasi musbatmi: {numbers.All(number => number > 0)}");
+Console.WriteLine($"Ichida toq sonlar bormi: {numbers.Any(number => number != 0)}");
+Console.WriteLine($"ichida 3 && 9 sonlari bormi: {numbers.Contains(3) && numbers.Contains(9)}");
