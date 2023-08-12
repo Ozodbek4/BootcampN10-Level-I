5cbf30bfde0097ecd27a9ae7546fcb9758eb76d4
Ozodbek4 <anvarjonovozodbek416@gmail.com>
2023-08-12 18:01:08 +0500

    HomeTask2

---
+++ N23Home4/Book.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace N23Home4
+{
+    internal class Book
+    {
+        public string Name { get; set; }
+        public int Rating { get; set; }
+        public string Author { get; set; }
+
+        public Book(string name, int rating, string author)
+        {
+            Name = name;
+            Rating = rating;
+            Author = author;
+        }
+    }
+}
---
+++ N23Home4/Program.cs
@@ -0,0 +1,27 @@
+﻿using N23Home4;
+
+List<Book> books = new List<Book>()
+{
+    new Book("Saodat Asri", 8, "Ahmad Lutfiy Qozonchi"),
+    new Book("Oltin Silsila", 10, "Shayx Muhammad Sodiq Muhammad Yusuf"),
+    new Book("O'tgan kunlar", 9, "Abdulla Qodiriy"),
+    new Book("Mehrobdan Chayon", 8, "Abdulla Qodiriy"),
+    new Book("Jinlar Bazimi", 4, "Abdulla Qodiriy"),
+    new Book("Tavsiri Hilol", 10, "Shayx Muhammad Sodiq Muhammad Yusuf")
+};
+
+//var newBook = books.OrderByDescending(book => book.Rating).ToList();
+
+//newBook.ForEach(book => Console.WriteLine($"{book.Name.PadRight(15)}" +
+//    $"Rayting: {book.Rating.ToString().PadRight(5)}{book.Author}"));
+//Console.WriteLine("\n");
+
+//var a = newBook.First(book => book.Author == "Abdulla Qodiriy");
+//var b = newBook.Last(book => book.Author == "Abdulla Qodiriy");
+//Console.WriteLine($"{a.Name} {a.Rating} {a.Author}");
+//Console.WriteLine($"{b.Name} {b.Rating} {b.Author}");
+
+var a = books.OrderBy(book => book.Rating).First(book => book.Author == "Abdulla Qodiriy");
+var b = books.OrderBy(book => book.Rating).Last(book => book.Author == "Abdulla Qodiriy");
+Console.WriteLine($"{a.Name} {a.Rating} {a.Author}");
+Console.WriteLine($"{b.Name} {b.Rating} {b.Author}");
